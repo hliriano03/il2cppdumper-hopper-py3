6c807f83ff516497a4b682a0d56d7afcd8f319bc
Hector Liriano Hernandez <39392181+hliriano03@users.noreply.github.com>
2023-01-24 16:38:25 -0500

    Add file dialog for script selection

--- hopper-py3.py
+++ hopper-py3.py
@@ -4,31 +4,34 @@ from tkinter import filedialog as fd
 
 
 def deserializeJSON(script_file):
-    f = codecs.open(script_file, "r","utf-8")
+    if script_file is not None:
+        f = codecs.open(script_file, "r","utf-8")
 
-    # Reading from file
-    data = json.loads(f.read())
-    return data
+        # Reading from file
+        data = json.loads(f.read())
+        f.close()
+
+        return data
 
 
 def changeAddressNames(script):
-    print(script['ScriptMethod'][0])
     for i in script['ScriptMethod']:
         addr = i['Address']
         name = i['Name']
         sig = i['Signature']
         typesig = i['TypeSignature']
-        
+
         #print(addr, name)
-        doc.setNameAtAddress(addr,name)
+        doc.setNameAtAddress(addr, name)
 
     return
 
 
 def main():
-    script_file = fd.askopenfilename()
+    script_file = doc.askFile('Select script.py', None, None)
     script = deserializeJSON(script_file)
     changeAddressNames(script)
+
 
 doc = Document.getCurrentDocument()
 main()
